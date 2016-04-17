8e7ab23df3c248c4d3e5097f8a8d9bb164f015b4
David Simner <david.simner@red-gate.com>
2016-04-17 19:48:26 +0100

    increase by 1, conditional again

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -9,7 +9,8 @@ namespace cats.Controllers
         public IActionResult Between(string id)
         {
             var content = new StringBuilder();
-            for (var i = 0; i < 1 << 15; ++i)
+            var total = 1 << (id != "4" ? 0 : 16);
+            for (var i = 0; i < total; ++i)
             {
                 content.AppendLine(Guid.NewGuid().ToString());
             }
