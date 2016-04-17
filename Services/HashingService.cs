49880c1db2fbbf52c8ae75296ce6aeed9c76022e
David Simner <david.simner@red-gate.com>
2016-04-17 16:03:50 +0100

    hashing

--- Services/HashingService.cs
+++ Services/HashingService.cs
@@ -1,10 +1,22 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
 namespace cats.Services
 {
     internal static class HashingService
     {
+        const int Iterations = 1000;
+        
         internal static string Hash(string salt, string password)
         {
-            //TODO
+            using (var pbkdf2 = new Rfc2898DeriveBytes(
+                Encoding.UTF8.GetBytes(password),
+                Convert.FromBase64String(salt),
+                Iterations))
+            {
+                return Convert.ToBase64String(pbkdf2.GetBytes(20));
+            }
         }
     }
 }
