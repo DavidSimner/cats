f441718f617556dac51f10e4d5135a580b35a262
David Simner <david.simner@red-gate.com>
2016-04-17 15:05:24 +0100

    a service to get the cookie value

---
+++ Services/CookieService.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using Microsoft.AspNet.Http;
+
+namespace cats.Services
+{
+    internal static class CookieService
+    {
+        private const string Key = "THE_SESSION_ID";
+        
+        internal static string GetCookieValue(HttpContext httpContext)
+        {
+            var cookie = httpContext.Request.Cookies[Key];
+            if (!cookie.Any())
+            {
+                var value = Guid.NewGuid().ToString("N");
+                httpContext.Response.Cookies.Append(Key, value);
+                return value;
+            }
+            else
+            {
+                return cookie.Single();
+            }
+        }
+    }
+}
